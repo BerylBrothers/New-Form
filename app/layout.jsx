785f3640f5e31e803143ecc7b49787d2a2aa4a5c
Jimmy Johnson <Daniel@Berylbrothers.com>
2025-02-10 18:50:20 -0800

    Stair Transition in progress

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header"
 import PageTransition from "../components/PageTransition";
+import StairTransition from "../components/StairTransition";
 
 const jetbrains_Mono = JetBrains_Mono({
   subsets: ["latin"], weight: ["100", "200", "300", "400",
@@ -17,7 +18,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${jetbrains_Mono.variable}`}>
          <Header />
+         <StairTransition />
          <PageTransition>
+         
          {children}
          </PageTransition>
       
