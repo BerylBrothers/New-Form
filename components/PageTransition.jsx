ba4ed41fff9331eea4b7fbf80402c10278799f18
Jimmy Johnson <Daniel@Berylbrothers.com>
2025-02-10 19:59:15 -0800

    Stair Transition Complete

--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -14,8 +14,8 @@ const PageTransition = ({children}) => {
          animate={{opacity:0, transition: {delay:1, duration:0.4, ease:"easeInOut"},}}
          className='h-screen w-screen fixed bg-primary top-0 pointer-events-none'/>
     {children}
-    </div>
 
+    </div>
     </AnimatePresence>
     
   
