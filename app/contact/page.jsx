01a379111080719047fcffc1e12f99e261c1310b
Jimmy Johnson <daniel@berylbrothers.com>
2025-02-22 12:30:23 -0800

    testing new form

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -33,114 +33,114 @@ const info = [
 
 const Contact = () => {
   
-  //Create state for the form data
-  const [formData, setFormData] = useState({
-    firstname: '',
-    lastname: '',
-    email: '',
-    phone: '',
-    service: '',  // This will store the selected service
-    message: ''
-  });
+  // //Create state for the form data
+  // const [formData, setFormData] = useState({
+  //   firstname: '',
+  //   lastname: '',
+  //   email: '',
+  //   phone: '',
+  //   service: '',  // This will store the selected service
+  //   message: ''
+  // });
 
-  // Handle form field changes
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+  // // Handle form field changes
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     [name]: value,
+  //   }));
+  // };
 
-  // Handle select change for "Select a service"
-  const handleServiceChange = (value) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      service: value,
-    }));
-  };
+  // // Handle select change for "Select a service"
+  // const handleServiceChange = (value) => {
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     service: value,
+  //   }));
+  // };
 
-  // Handle form submission
-  const sendEmail = async (e) => {
-    e.preventDefault();
+  // // Handle form submission
+  // const sendEmail = async (e) => {
+  //   e.preventDefault();
 
-    // Make a POST request to the API route
-    const response = await fetch("/api/contact", {
-      method: "POST",
-      body: JSON.stringify(formData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  //   // Make a POST request to the API route
+  //   const response = await fetch("/api/contact", {
+  //     method: "POST",
+  //     body: JSON.stringify(formData),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
 
-    if (response.ok) {
-      alert("Email sent successfully!");
-    } else {
-      alert("Error sending email.");
-    }
-  };
+  //   if (response.ok) {
+  //     alert("Email sent successfully!");
+  //   } else {
+  //     alert("Error sending email.");
+  //   }
+  // };
 
-  return (
-    <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }} className='py-6'>
-      <div className="container mx-auto">
-        <div className='flex flex-col xl:flex-row gap-[30px]'>
-          {/* form */}
-          <div className='xl:-[54%] order-2 xl:order-none'>
-            <form className="flex flex-col gap-6 p-10 bg-blue-600 rounded-xl" onSubmit={sendEmail}>
-              <h3 className='text-white'>Let's Work Together!</h3>
-              <p>I’m passionate about creating user-friendly, high-quality websites and web applications. With expertise in modern technologies and design, I’m committed to delivering solutions that match your vision and goals. Let’s work together to bring your ideas to life with clean, efficient code.</p>
+  // return (
+  //   <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }} className='py-6'>
+  //     <div className="container mx-auto">
+  //       <div className='flex flex-col xl:flex-row gap-[30px]'>
+  //         {/* form */}
+  //         <div className='xl:-[54%] order-2 xl:order-none'>
+  //           <form className="flex flex-col gap-6 p-10 bg-blue-600 rounded-xl" onSubmit={sendEmail}>
+  //             <h3 className='text-white'>Let's Work Together!</h3>
+  //             <p>I’m passionate about creating user-friendly, high-quality websites and web applications. With expertise in modern technologies and design, I’m committed to delivering solutions that match your vision and goals. Let’s work together to bring your ideas to life with clean, efficient code.</p>
 
-              {/* input */}
-              <div className='text-white grid grid-cols-1 md:grid-cols-2 gap-6'>
-                <Input name="firstname" type="text" placeholder="First Name" value={formData.firstname} onChange={handleInputChange} />
-                <Input name="lastname" type="text" placeholder="Last Name" value={formData.lastname} onChange={handleInputChange} />
-                <Input name="email" type="email" placeholder="Email Address" value={formData.email} onChange={handleInputChange} />
-                <Input name="phone" type="phone" placeholder="Phone Number" value={formData.phone} onChange={handleInputChange} />
-              </div>
+  //             {/* input */}
+  //             <div className='text-white grid grid-cols-1 md:grid-cols-2 gap-6'>
+  //               <Input name="firstname" type="text" placeholder="First Name" value={formData.firstname} onChange={handleInputChange} />
+  //               <Input name="lastname" type="text" placeholder="Last Name" value={formData.lastname} onChange={handleInputChange} />
+  //               <Input name="email" type="email" placeholder="Email Address" value={formData.email} onChange={handleInputChange} />
+  //               <Input name="phone" type="phone" placeholder="Phone Number" value={formData.phone} onChange={handleInputChange} />
+  //             </div>
 
-              {/* Select dropdown */}
-              <Select value={formData.service} onValueChange={handleServiceChange}>
-                <SelectTrigger className="w-full rounded-xl">
-                  <SelectValue placeholder="Select a service" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Select a service</SelectLabel>
-                    <SelectItem value="Web-Dev">Web Development</SelectItem>
-                    <SelectItem value="Ui/UX">UI/UX</SelectItem>
-                    <SelectItem value="Logo-Design">Logo Design</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+  //             {/* Select dropdown */}
+  //             <Select value={formData.service} onValueChange={handleServiceChange}>
+  //               <SelectTrigger className="w-full rounded-xl">
+  //                 <SelectValue placeholder="Select a service" />
+  //               </SelectTrigger>
+  //               <SelectContent>
+  //                 <SelectGroup>
+  //                   <SelectLabel>Select a service</SelectLabel>
+  //                   <SelectItem value="Web-Dev">Web Development</SelectItem>
+  //                   <SelectItem value="Ui/UX">UI/UX</SelectItem>
+  //                   <SelectItem value="Logo-Design">Logo Design</SelectItem>
+  //                 </SelectGroup>
+  //               </SelectContent>
+  //             </Select>
 
-              {/* Text Area */}
-              <Textarea name="message" className="h-[200px]" placeholder="Type your message here." value={formData.message} onChange={handleInputChange} />
+  //             {/* Text Area */}
+  //             <Textarea name="message" className="h-[200px]" placeholder="Type your message here." value={formData.message} onChange={handleInputChange} />
 
-              {/* button */}
-              <Button size="md" className="max-w-40 bg-white hover:bg-primary hover:text-white" type="submit">Send Message</Button>
-            </form>
-          </div>
+  //             {/* button */}
+  //             <Button size="md" className="max-w-40 bg-white hover:bg-primary hover:text-white" type="submit">Send Message</Button>
+  //           </form>
+  //         </div>
 
-          {/* info */}
-          <div className='flex-1 flex items-center justify-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0'>
-            <ul className='flex flex-col gap-10'>
-              {info.map((item, index) => (
-                <li key={index} className='flex items-center  gap-6 bg-white/10 rounded-xl p-3 pr-7'>
-                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] text-primary rounded-xl flex items-center justify-center">
-                    <div className='text-[28px]'>{item.icon}</div>
-                  </div>
-                  <div className='flex-1 justify-center items-center'>
-                    <p>{item.title}</p>
-                    <h3 className='text-xl '>{item.description}</h3>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </motion.section>
-  );
+  //         {/* info */}
+  //         <div className='flex-1 flex items-center justify-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0'>
+  //           <ul className='flex flex-col gap-10'>
+  //             {info.map((item, index) => (
+  //               <li key={index} className='flex items-center  gap-6 bg-white/10 rounded-xl p-3 pr-7'>
+  //                 <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] text-primary rounded-xl flex items-center justify-center">
+  //                   <div className='text-[28px]'>{item.icon}</div>
+  //                 </div>
+  //                 <div className='flex-1 justify-center items-center'>
+  //                   <p>{item.title}</p>
+  //                   <h3 className='text-xl '>{item.description}</h3>
+  //                 </div>
+  //               </li>
+  //             ))}
+  //           </ul>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </motion.section>
+  // );
 
   // const [formData, setFormData] = useState({
   //   firstname: '',
@@ -192,6 +192,173 @@ const Contact = () => {
   //       alert('Error sending email.');
   //     });
   // };
+
+
+
+
+
+
+
+
+
+
+//   async function handleSubmit(event) {
+//     event.preventDefault();
+//     const formData = new FormData(event.target);
+
+//     formData.append("access_key", "4da46f02-baba-4174-ad4b-62201ba4518d");
+
+//     const object = Object.fromEntries(formData);
+//     const json = JSON.stringify(object);
+
+//     const response = await fetch("https://api.web3forms.com/submit", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Accept: "application/json"
+//         },
+//         body: json
+//     });
+//     const result = await response.json();
+//     if (result.success) {
+//         console.log(result);
+//     }
+// }
+
+// return (
+// <>
+//   <form onSubmit={handleSubmit}>
+//     <input type="text" name="name"/>
+//     <input type="email" name="email"/>
+//     <textarea name="message"></textarea>
+//     <button type="submit">Submit Form</button>
+//   </form>
+// </>
+// );
+
+
+
+
+
+const [formData, setFormData] = useState({
+  firstname: '',
+  lastname: '',
+  email: '',
+  phone: '',
+  service: '',
+  message: ''
+});
+
+// Handle form field changes
+const handleInputChange = (e) => {
+  const { name, value } = e.target;
+  setFormData((prevData) => ({
+    ...prevData,
+    [name]: value,
+  }));
+};
+
+// Handle select change for "Select a service"
+const handleServiceChange = (value) => {
+  setFormData((prevData) => ({
+    ...prevData,
+    service: value,
+  }));
+};
+
+// Handle form submission
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+
+  formData.append("access_key", "4da46f02-baba-4174-ad4b-62201ba4518d");
+
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+
+  const response = await fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: json
+  });
+
+  const result = await response.json();
+  if (result.success) {
+    console.log(result);
+    alert("Email sent successfully!");
+  } else {
+    alert("Error sending email.");
+  }
+};
+
+return (
+  <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }} className='py-6'>
+    <div className="container mx-auto">
+      <div className='flex flex-col xl:flex-row gap-[30px]'>
+        {/* form */}
+        <div className='xl:-[54%] order-2 xl:order-none'>
+          <form className="flex flex-col gap-6 p-10 bg-blue-600 rounded-xl" onSubmit={handleSubmit}>
+            <h3 className='text-white'>Let's Work Together!</h3>
+            <p>I’m passionate about creating user-friendly, high-quality websites and web applications. With expertise in modern technologies and design, I’m committed to delivering solutions that match your vision and goals. Let’s work together to bring your ideas to life with clean, efficient code.</p>
+
+            {/* input fields */}
+            <div className='text-white grid grid-cols-1 md:grid-cols-2 gap-6'>
+              <Input name="firstname" type="text" placeholder="First Name" value={formData.firstname} onChange={handleInputChange} />
+              <Input name="lastname" type="text" placeholder="Last Name" value={formData.lastname} onChange={handleInputChange} />
+              <Input name="email" type="email" placeholder="Email Address" value={formData.email} onChange={handleInputChange} />
+              <Input name="phone" type="phone" placeholder="Phone Number" value={formData.phone} onChange={handleInputChange} />
+            </div>
+
+            {/* Select dropdown */}
+            <Select value={formData.service} onValueChange={handleServiceChange}>
+              <SelectTrigger className="w-full rounded-xl">
+                <SelectValue placeholder="Select a service" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Select a service</SelectLabel>
+                  <SelectItem value="Web-Dev">Web Development</SelectItem>
+                  <SelectItem value="Ui/UX">UI/UX</SelectItem>
+                  <SelectItem value="Logo-Design">Logo Design</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+
+            {/* Text Area */}
+            <Textarea name="message" className="h-[200px]" placeholder="Type your message here." value={formData.message} onChange={handleInputChange} />
+
+            {/* Submit button */}
+            <Button size="md" className="max-w-40 bg-white hover:bg-primary hover:text-white" type="submit">Send Message</Button>
+          </form>
+        </div>
+
+        {/* info section */}
+        <div className='flex-1 flex items-center justify-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0'>
+          <ul className='flex flex-col gap-10'>
+            {info.map((item, index) => (
+              <li key={index} className='flex items-center gap-6 bg-white/10 rounded-xl p-3 pr-7'>
+                <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] text-primary rounded-xl flex items-center justify-center">
+                  <div className='text-[28px]'>{item.icon}</div>
+                </div>
+                <div className='flex-1'>
+                  <p>{item.title}</p>
+                  <h3 className='text-xl'>{item.description}</h3>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  </motion.section>
+);
+
+
+
+
 
   // return (
   //   <form onSubmit={sendEmail}>
